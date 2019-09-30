9c11e0272ea4d8e166f1df464bf7b27b6ace4dbb
gregorpf <gregor.pfirter@gmail.com>
2019-09-30 22:37:48 +0200

    changed radius

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -533,7 +533,7 @@
   #endif
 
   // Print surface diameter/2 minus unreachable space (avoid collisions with vertical towers).
-  #define DELTA_PRINTABLE_RADIUS 140.0 // mm
+  #define DELTA_PRINTABLE_RADIUS 110.0 // mm
 
   // Center-to-center distance of the holes in the diagonal push rods.
   #define DELTA_DIAGONAL_ROD 258.0 // mm
